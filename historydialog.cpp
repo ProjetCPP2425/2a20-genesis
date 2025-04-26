2515e8f15e46bf17a55d7415ee8e45b7fc92e763
imenbenjemaa04 <benjemaa.imen@esprit.tn>
2025-04-26 11:26:56 +0100

    aaaa

--- historydialog.cpp
+++ historydialog.cpp
@@ -1,4 +1,4 @@
-// HistoryDialog.cpp
+
 #include "HistoryDialog.h"
 #include <QPushButton>
 
@@ -39,15 +39,9 @@ HistoryDialog::HistoryDialog(QWidget *parent) : QDialog(parent)
         }
     )");
 
-<<<<<<< HEAD
     layout = new QVBoxLayout(this);
-=======
-<<<<<<< HEAD
     layout = new QVBoxLayout(this);
-=======
     layout = new QVBoxLayout(this);//layout vertival
->>>>>>> 8a1c2a3 (final)
->>>>>>> 983537a7dd52ff7ff4e5d09ae92c7113c9018da9
     titleLabel = new QLabel("Event History Details:", this);
 
     historyListWidget = new QListWidget(this);
